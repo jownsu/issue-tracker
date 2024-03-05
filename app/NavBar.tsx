79c7a420170d79d92b742da254286f928ea1d30b
unknown <jhonesdigno666@gmail.com>
2024-03-05 11:50:39 +0800

    Adjusted navbar UI

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -7,6 +7,7 @@ import { usePathname } from "next/navigation";
 /* PLUGINS */
 import classNames from "classnames";
 import { AiFillBug } from "react-icons/ai";
+import { Container, Flex } from "@radix-ui/themes";
 
 const NavBar = () => {
 	const links = [
@@ -23,26 +24,30 @@ const NavBar = () => {
 	const pathname = usePathname();
 
 	return (
-		<nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-			<Link href={"/"}>
-				<AiFillBug />
-			</Link>
-			<ul className="flex space-x-6">
-				{links.map((link) => (
-					<li key={link.href}>
-						<Link
-							href={link.href}
-							className={classNames({
-								"text-zinc-900": pathname === link.href,
-								"text-zinc-500": pathname !== link.href,
-								"hover:text-zinc-800 transition-colors": true
-							})}
-						>
-							{link.label}
-						</Link>
-					</li>
-				))}
-			</ul>
+		<nav className="border-b mb-5 px-5 py-3">
+			<Container>
+				<Flex align={"center"} gap={"3"}>
+					<Link href={"/"}>
+						<AiFillBug />
+					</Link>
+					<ul className="flex space-x-6">
+						{links.map((link) => (
+							<li key={link.href}>
+								<Link
+									href={link.href}
+									className={classNames({
+										"text-zinc-900": pathname === link.href,
+										"text-zinc-500": pathname !== link.href,
+										"hover:text-zinc-800 transition-colors": true
+									})}
+								>
+									{link.label}
+								</Link>
+							</li>
+						))}
+					</ul>
+				</Flex>
+			</Container>
 		</nav>
 	);
 };
