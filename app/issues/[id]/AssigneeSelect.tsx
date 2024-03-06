54e7ade6d930881280bd48da68e2d1d9a5bfced4
unknown <jhonesdigno666@gmail.com>
2024-03-06 19:00:26 +0800

    Build api for fetching users and populate the assignee select compoent with users

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,15 +1,38 @@
 "use client";
 
+/* NEXT */
+import { useEffect, useState } from "react";
+
+/* CLIENT */
+import { User } from "@prisma/client";
+
+/* PLUGINS */
 import { Select } from "@radix-ui/themes";
+import axios from "axios";
 
 const AssigneeSelect = () => {
+	const [users, setUsers] = useState<User[]>([]);
+
+	useEffect(() => {
+		const fetchUsers = async () => {
+			const response = await axios.get<User[]>("/api/users");
+			setUsers(response.data);
+		};
+
+		fetchUsers();
+	}, []);
+
 	return (
 		<Select.Root>
 			<Select.Trigger placeholder="Assign..." />
 			<Select.Content>
 				<Select.Group>
 					<Select.Label>Suggestions</Select.Label>
-					<Select.Item value="1">Jhones Digno</Select.Item>
+					{users.map((user) => (
+						<Select.Item key={user.id} value={user.id}>
+							{user.name}
+						</Select.Item>
+					))}
 				</Select.Group>
 			</Select.Content>
 		</Select.Root>
