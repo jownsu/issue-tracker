9cb38f83ab974cd15ef4165c6c50b67f487da0ac
unknown <jhonesdigno666@gmail.com>
2024-03-07 12:16:57 +0800

    Order filters

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -16,36 +16,41 @@ interface Props {
 	searchParams: { status: Status; orderBy: keyof Issue };
 }
 
+const columns: {
+	label: String;
+	value: keyof Issue;
+	className?: String;
+}[] = [
+	{
+		label: "Issue",
+		value: "title"
+	},
+	{
+		label: "Status",
+		value: "status",
+		className: "hidden md:table-cell"
+	},
+	{
+		label: "Created",
+		value: "createdAt",
+		className: "hidden md:table-cell"
+	}
+];
+
 const IssuesPage = async ({ searchParams }: Props) => {
 	const statuses = Object.values(Status);
 	const status = statuses.includes(searchParams.status)
 		? searchParams.status
 		: undefined;
 
-	const issues = await prisma.issue.findMany({
-		where: { status }
-	});
+	const orderBy = columns.find((column) => column.value === searchParams.orderBy)
+		? { [searchParams.orderBy]: "asc" }
+		: undefined;
 
-	const columns: {
-		label: String;
-		value: keyof Issue;
-		className?: String;
-	}[] = [
-		{
-			label: "Issue",
-			value: "title"
-		},
-		{
-			label: "Status",
-			value: "status",
-			className: "hidden md:table-cell"
-		},
-		{
-			label: "Created",
-			value: "createdAt",
-			className: "hidden md:table-cell"
-		}
-	];
+	const issues = await prisma.issue.findMany({
+		where: { status },
+		orderBy
+	});
 
 	return (
 		<div>
