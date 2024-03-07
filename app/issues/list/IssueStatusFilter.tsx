b9ae847de485942d40650ef801ec09408c31a80a
unknown <jhonesdigno666@gmail.com>
2024-03-07 11:38:32 +0800

    Implement filtering of issues

--- app/issues/list/IssueStatusFilter.tsx
+++ app/issues/list/IssueStatusFilter.tsx
@@ -3,21 +3,34 @@
 /* PLUGINS */
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
+import { useRouter, useSearchParams } from "next/navigation";
 
 const statuses: { label: string; value?: Status }[] = [
-	{ label: "All" },
+	{ label: "All", value: undefined },
 	{ label: "Open", value: "OPEN" },
-	{ label: "In progress", value: "IN_PROGRESS" },
+	{ label: "In Progress", value: "IN_PROGRESS" },
 	{ label: "Closed", value: "CLOSED" }
 ];
 
 const IssueStatusFilter = () => {
+	const router = useRouter();
+	const params = useSearchParams();
+
+	const selectedStatus = statuses.find(
+		(status) => status.value === params.get("status")
+	);
+
 	return (
-		<Select.Root>
+		<Select.Root
+			onValueChange={(status) => {
+				router.push(`/issues/list${status.trim() ? `?status=${status}` : ""}`);
+			}}
+			defaultValue={selectedStatus?.value || ""}
+		>
 			<Select.Trigger placeholder="Filter by status..." />
 			<Select.Content>
-				{statuses.map((status) => (
-					<Select.Item key={status.value} value={status.value || " "}>
+				{statuses.map((status, key) => (
+					<Select.Item key={key} value={status.value || " "}>
 						{status.label}
 					</Select.Item>
 				))}
--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -9,9 +9,21 @@ import IssueActions from "./IssueActions";
 import { IssueStatusBadge } from "@/app/components";
 
 import Link from "../../components/Link";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-	const issues = await prisma.issue.findMany();
+interface Props {
+	searchParams: { status: Status };
+}
+
+const IssuesPage = async ({ searchParams }: Props) => {
+	const statuses = Object.values(Status);
+	const status = statuses.includes(searchParams.status)
+		? searchParams.status
+		: undefined;
+
+	const issues = await prisma.issue.findMany({
+		where: { status }
+	});
 
 	return (
 		<div>
